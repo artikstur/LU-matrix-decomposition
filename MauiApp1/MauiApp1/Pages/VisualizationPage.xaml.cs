9142283a83434a23c0689d5c32ba653e2182e24c
artur <arturgurin39@gmail.com>
2024-04-22 15:15:08 +0300

    initial commit

---
+++ MauiApp1/MauiApp1/Pages/VisualizationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Pages;
+
+public partial class VisualizationPage : ContentPage
+{
+    private int[,] _matrix;
+    public VisualizationPage(int[,] matrix)
+    {
+        _matrix = matrix;
+        InitializeComponent();
+        Title = "Визуализация";
+    }
+}
---
+++ MauiApp1/MauiApp1/ViewModel/EntryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiApp1.ViewModel;
+
+public class EntryViewModel: INotifyPropertyChanged
+{
+	private string _text;
+    
+	public string Text
+    {
+        get => _text;
+		set
+        {
+            _text = value;
+            OnPropertyChanged(nameof(value));
+        }
+	}
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        Console.WriteLine(_text);
+    }
+}
